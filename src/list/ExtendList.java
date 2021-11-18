762d3a7a04d18c44c36e1950c801e460b159cffd
BabSarg <babsargs@gmail.com>
2021-11-18 22:52:04 +0400

    initial commit

---
+++ src/list/ExtendList.java
@@ -0,0 +1,78 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.Supplier;
+
+public class ExtendList<T> extends ArrayList<T> {
+
+    public <R> List<R> map(Function<T, R> function) {
+        List<R> list = new ArrayList<>();
+        for (Object elem : toArray()) {
+            list.add(function.apply((T) elem));
+        }
+        return list;
+    }
+
+    public void fill(int capacity, Supplier<T> supplier) {
+        for (int i = 0; i < capacity; i++) {
+            add(supplier.get());
+        }
+    }
+
+    public boolean forall(Predicate<T> predicate) {
+        int trueElementsCount = 0;
+        for (Object elem : toArray()) {
+            boolean test = predicate.test((T) elem);
+            if (test) {
+                trueElementsCount++;
+            }
+        }
+        return trueElementsCount == size();
+    }
+
+    public List<T>[] partition(Predicate<T> predicate) {
+        List<T> list1 = new ArrayList<>();
+        List<T> list2 = new ArrayList<>();
+        for (Object elem : toArray()) {
+            boolean test = predicate.test((T) elem);
+            if (test) {
+                list1.add((T) elem);
+            } else {
+                list2.add((T) elem);
+            }
+        }
+        return new List[]{list1, list2};
+    }
+
+    public T reduce(T elem,BinaryOperator<T> binaryOperator){
+        for (Object element : toArray()) {
+            elem = binaryOperator.apply(elem, (T) element);
+        }
+        return elem;
+    }
+
+
+    public static void main(String[] args) {
+
+        ExtendList<Integer> extendList = new ExtendList<>();
+
+        extendList.fill(100,() -> new Random().nextInt());
+
+        List<Integer> s = extendList.map(integer -> integer * integer);
+        System.out.println(s);
+        List<String> map = extendList.map(integer -> "number = " + integer);
+        System.out.println(map);
+
+        List<Integer>[] result = extendList.partition(integer -> integer % 2 == 0);
+
+        Integer sum = extendList.reduce(1, (integer, integer2) -> integer * integer2);
+        System.out.println(sum);
+
+
+    }
+}
